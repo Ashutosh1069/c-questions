a0860ff750d26b9f10feea4d6d1f01aac7e715e6
Juhi Dwivedi <91658716+Inconsequential-24@users.noreply.github.com>
2022-10-23 14:47:11 +0530

    Just Made It More User Friendly
    Minor changes

--- add_two_number.c
+++ add_two_number.c
@@ -3,12 +3,16 @@ int main() {
 
     int number1, number2, sum;
     
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    printf("Enter first number: \n");
+    scanf("%d", &number1);
+    
+    printf("Enter second number: \n");
+    scanf("%d", &number2);
 
     // calculating sum
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
+    
     return 0;
 }
