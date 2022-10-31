b1d2f01ceaee212f64f4d967faf842a79dcbd37a
kumud0508 <115732897+kumud0508@users.noreply.github.com>
2022-10-31 18:02:23 +0530

    added number 3
    random

--- add_two_number.c
+++ add_two_number.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int number1, number2, number3, sum;
     
     printf("Enter first number: \n");
     scanf("%d", &number1);
     
     printf("Enter second number: \n");
     scanf("%d", &number2);
+    
+    printf("Enter third number: \n");
+    scanf("%d", &number3);
 
     // calculating sum
-    sum = number1 + number2;      
+    sum = number1 + number2 + number3;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d +%d = %d", number1, number2, number3, sum);
     
     printf("Maths is Fun!");
+    printf("Naah! Just Kidding");
     
     return 0;
 }
